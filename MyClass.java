b05b09e1ab33d6a107ef197154401097853bbe59
treesheal <treesheal@users.noreply.github.com>
2016-09-21 20:35:01 -0500

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyClass {
+
+public static void main(String[] arg){
+    String c = "110010"; // as binary
+    int decimalValue = Integer.parseInt(c, 2);
+    System.out.println(decimalValue);
+
+    }
+}
+
+
+
